3fcf0af96c21625e290b77bbc51ddbfd7bb21a5d
GitHub Action <action@github.com>
2025-06-12 19:28:54 +0000

    Update eir formula to v0.1.48

--- Formula/eir.rb
+++ Formula/eir.rb
@@ -1,15 +1,15 @@
 class Eir < Formula
   desc "EXIF-based image renamer and RAW format converter"
   homepage "https://github.com/alexbigkid/eir"
-  version "0.1.47"
+  version "0.1.48"
   
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/alexbigkid/eir/releases/download/v#{version}/eir-#{version}-macos-universal"
-      sha256 "217a807b8e6bb1a04d0929b6d53730fd57f0f6729d1f9ad20d7c0f8792724427"
+      sha256 "11d286af440b61520b0899807f4602e9eccc718d7e9e8ff8993df94cc5f58573"
     else
       url "https://github.com/alexbigkid/eir/releases/download/v#{version}/eir-#{version}-macos-universal"
-      sha256 "217a807b8e6bb1a04d0929b6d53730fd57f0f6729d1f9ad20d7c0f8792724427"
+      sha256 "11d286af440b61520b0899807f4602e9eccc718d7e9e8ff8993df94cc5f58573"
     end
   end
 
