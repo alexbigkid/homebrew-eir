6762bdc46dc077234869b2a02f34c1dbb504bfff
GitHub Action <action@github.com>
2025-07-10 10:58:28 +0000

    Update eir formula to v0.3.6

--- Formula/eir.rb
+++ Formula/eir.rb
@@ -1,11 +1,12 @@
 class Eir < Formula
   desc "EXIF-based image renamer and RAW format converter"
   homepage "https://github.com/alexbigkid/eir"
-  version "0.3.3"
+  version "0.3.6"
   url "https://github.com/alexbigkid/eir/releases/download/v#{version}/eir-#{version}-macos-universal"
-  sha256 "70cf9b048f9ab279e511f242aa27cf39484fd08a8719663582ea3437ce217511"
+  sha256 "952a8af5e4f424deed4f551d5a00db1e5a53a5b369bd582c5beaf900f2038504"
 
   depends_on "exiftool"
+  depends_on cask: "adobe-dng-converter"
 
   def install
     bin.install "eir-#{version}-macos-universal" => "eir"
