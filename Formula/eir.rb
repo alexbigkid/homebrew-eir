f5dc7b8777cb40a5e8058eba8d5184c8a21ca0ee
GitHub Action <action@github.com>
2025-07-10 12:51:53 +0000

    Update eir formula to v0.3.7

--- Formula/eir.rb
+++ Formula/eir.rb
@@ -1,15 +1,22 @@
 class Eir < Formula
   desc "EXIF-based image renamer and RAW format converter"
   homepage "https://github.com/alexbigkid/eir"
-  version "0.3.6"
+  version "0.3.7"
   url "https://github.com/alexbigkid/eir/releases/download/v#{version}/eir-#{version}-macos-universal"
-  sha256 "952a8af5e4f424deed4f551d5a00db1e5a53a5b369bd582c5beaf900f2038504"
+  sha256 "226bc5482ac77ef17985d4e6fb1bc2d375be403d5907cc94b716d3ed424de04f"
 
   depends_on "exiftool"
-  depends_on cask: "adobe-dng-converter"
 
   def install
     bin.install "eir-#{version}-macos-universal" => "eir"
+  end
+
+  def caveats
+    <<~EOS
+      This formula requires Adobe DNG Converter for RAW to DNG conversion on macOS.
+      Install it with:
+        brew install --cask adobe-dng-converter
+    EOS
   end
 
   test do
